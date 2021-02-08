f662ac5d7fe60e37edba3b782d53d915e53bb00a
chamika <chamika.hasanthi@axiatadigitallabs.com>
2021-02-08 20:13:43 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import {
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  ActivityIndicator,
+} from "react-native";
+
+export default class DynamicUiBuilder extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Japan </Text>
+      </View>
+    );
+  }
+}
