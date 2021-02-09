7a1a6a4b6f5e6fa9e971a447a3af4291f7cf2b86
chamika <chamika.hasanthi@axiatadigitallabs.com>
2021-02-09 12:23:59 +0530

    Build jsx component from json.

--- index.js
+++ index.js
@@ -1,18 +1,69 @@
 import React, { Component } from "react";
-import {
-  Text,
-  View,
-  Image,
-  TouchableOpacity,
-  ActivityIndicator,
-} from "react-native";
+import { Text, View, TouchableOpacity, Image, ScrollView } from "react-native";
 
-export default class DynamicUiBuilder extends Component {
-  render() {
-    return (
-      <View>
-        <Text> Japan </Text>
-      </View>
-    );
+var dataType;
+
+const getCustomComponent = (payload) => {
+  dataType = getDataType(payload.data);
+  return parse(payload.data, true);
+};
+
+const onButtonPress = (prop) => {
+  console.warn("on button press -->", prop);
+};
+
+const getDataType = (dataStr) => {
+  try {
+    if (typeof dataStr === "string" && dataStr.startsWith("<")) {
+      return "xml";
+    } else {
+      return "json";
+    }
+  } catch (ex) {
+    return "json";
   }
-}
+};
+
+const getComponent = (type) => {
+  switch (type) {
+    case "ScrollView":
+      return ScrollView;
+    case "View":
+      return View;
+    case "Text":
+      return Text;
+    case "TouchableOpacity":
+      return TouchableOpacity;
+    case "Image":
+      return Image;
+    case "Test":
+      return Test;
+    default:
+      break;
+  }
+};
+
+const parse = (data, flag) => {
+  const { type, children, props, text, func } = data;
+
+  const Type = getComponent(type);
+
+  let childrenList = [];
+  childrenList = children;
+
+  return (
+    <Type
+      {...props}
+      onPress={func !== undefined ? () => onButtonPress(func) : undefined}
+    >
+      {children !== undefined &&
+        (dataType === "json"
+          ? Array.isArray(children)
+            ? children.map((item) => parse(item))
+            : children
+          : parse(children))}
+    </Type>
+  );
+};
+
+export { getCustomComponent };
